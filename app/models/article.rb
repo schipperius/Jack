8b1cfbc641d913fa4ea01128553f508f5e94b3c6
Mark Schipper <schipperius@gmail.com>
2012-04-28 13:04:49 -0600

    fix article index and show views and model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ActiveRecord::Base
 
   scope :recent, order('position DESC')
 
+  validates :title, :introduction, :description, :footnotes, presence: true
+  validates :title, uniqueness: true
+
   def long_title
     "#{title} - #{published_at}"
   end
@@ -12,6 +15,14 @@ class Article < ActiveRecord::Base
 
   def next
     self.class.where("position > ?", position).order("position").first
+  end
+
+  def last_published?
+    self == self.class.published.last
+  end
+
+  def published?
+    published_at <= Time.zone.now
   end
 
 end
