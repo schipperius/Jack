9ffdb9567f23b43ac61a28faab3c5744b7ded0a6
Mark Schipper <schipperius@gmail.com>
2012-04-28 06:46:49 -0600

    destroy old db, create new db

--- db/migrate/20120425014426_remove_comments_count_from_articles.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCommentsCountFromArticles < ActiveRecord::Migration
-  def up
-    remove_column :articles, :comments_count
-      end
-
-  def down
-    add_column :articles, :comments_count, :integer
-  end
-end
