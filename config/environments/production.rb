8167e520580771e0df71764c3c7df48691db018d
Mark Schipper <schipperius@gmail.com>
2012-05-14 20:28:44 -0600

    fixing heroku javaScript issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Jack::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  
+  config.assets.precompile += %w( application.js )
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
